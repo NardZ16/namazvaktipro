b387884c375f93ef305195baef08f0f1eeb2db7f
NardZ16 <mchtaydn@hotmail.com>
2025-11-27 02:20:36 +0300

    fix podfile capacitor syntax

--- fix-ios.js
+++ fix-ios.js
@@ -4,17 +4,18 @@ const path = require('path');
 const podfileDir = path.join(__dirname, 'ios', 'App');
 const podfilePath = path.join(podfileDir, 'Podfile');
 
+// Correct Capacitor Podfile structure using pods_helpers
 const podfileContent = `
+require_relative '../../node_modules/@capacitor/ios/scripts/pods_helpers'
+
 platform :ios, '13.0'
 use_frameworks!
 
 target 'App' do
-  config = use_native_modules!
-
+  capacitor_pods
+  
   # Pin Google Mobile Ads SDK to 10.14.0 to fix build errors with Capacitor AdMob plugin
   pod 'Google-Mobile-Ads-SDK', '~> 10.14.0'
-
-  use_frameworks!
 end
 `;
 
